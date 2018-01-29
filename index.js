e86dff090c108249d7f15366c750d147a6679e8e
AStudentOfCS <doduykhiem@gmail.com>
2018-01-29 17:35:52 +0700

    Add lowdb, image render with Stream

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const _ = require('lodash');
+const lowdb = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
 const multer = require('multer');
 require('dotenv').config();
 
+const fs = require('fs');
 const path = require('path');
 const app = express();
 
 const packageJson = require('./package.json');
+
+const adapter = new FileAsync('db.json');
+const db = (async connection => {
+  const dbConnection = await connection;
+  await dbConnection.defaults({ resource: [], users: [] }).write();
+  return dbConnection;
+})(lowdb(adapter));
 
 app.get('/', (req, res) => {
   res.json(
     _.pick(packageJson, ['name', 'version', 'description', 'author', 'license'])
   );
 });
-
-// Upload images
-// const allowTypes = ['image/png', 'image/jpeg', 'image/gif'];
-// const uploadConfig = {
-//   fields: 17,
-//   files: 17,
-//   fileSize: 100 * 1048576,
-//   parts: 17
-// };
 
 const allowTypes = process.env.ALLOW_TYPES.split(',').map(type => type.trim());
 const uploadConfig = {
@@ -50,8 +51,66 @@ const fileFilter = (req, { mimetype }, cb) =>
 
 const uploader = multer({ storage, fileFilter, limits: uploadConfig });
 
-app.post('/upload', uploader.array('images'), (req, res) => {
-  res.json({ images: req.files });
+// Serve images
+app.get('/image/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const imgPath = path.resolve(__dirname, process.env.FOLDER_RESOURCE, id);
+
+    if (!fs.existsSync(imgPath)) {
+      throw new Error(`Image #${id} is not exist.`);
+    }
+
+    const imageStream = fs.createReadStream(imgPath);
+
+    return imageStream.pipe(res);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+// Errors handler
+app.use((err, req, res, next) => {
+  const message =
+    process.env.NODE_ENV !== 'production'
+      ? err.message
+      : 'An error encountered while processing images';
+
+  res.status(500).json({ message });
+
+  return next();
+});
+
+// Upload images and save in database
+app.post('/upload', uploader.array('images'), async ({ files }, res) => {
+  const dbInstance = await db;
+
+  const insertQueue = [];
+  const images = [];
+
+  _.each(files, ({ filename, path: imagePath, size }) => {
+    // Insert image info to db
+    insertQueue.push(
+      dbInstance
+        .get('resource')
+        .push({
+          id: filename,
+          name: filename,
+          path: imagePath,
+          size
+        })
+        .write()
+    );
+
+    // Prepare data to return to client
+    images.push({
+      name: filename
+    });
+  });
+
+  await Promise.all(insertQueue);
+
+  res.json({ images });
 });
 
 const port = process.env.PORT || 9999;
